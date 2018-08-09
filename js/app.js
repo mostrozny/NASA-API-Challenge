87f48bb5f4c3532021c0f0899cec0be05fbbe73d
mostrozny <ostrozny.michal@gmail.com>
2018-08-09 19:07:06 +0200

    updates

--- js/app.js
+++ js/app.js
@@ -1,39 +1,54 @@
 //api key: uPEwNrk9updnhBTYgoXZiLinsuV9XMoOmyJYSnZi
+$(() => {
 
+    const nasaUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100&page=2';
+    const backgroundNasa = 'https://api.nasa.gov/planetary/apod';
+    const apiKey = 'api_key=uPEwNrk9updnhBTYgoXZiLinsuV9XMoOmyJYSnZi';
+    let image = '';
 
-const nasaUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000';
-const backgroundNasa = 'https://api.nasa.gov/planetary/apod';
-const apiKey = 'api_key=uPEwNrk9updnhBTYgoXZiLinsuV9XMoOmyJYSnZi';
-let image = '';
+    const loadContent = () => {
+        $.ajax({
+            url: nasaUrl + '&' + apiKey,
+        }).done(function (response) {
+            console.log(response);
+        }).fail(function (error) {
+            console.log(error);
+        })
+    }
 
-const loadContent = () => {
-    $.ajax({
-        url: nasaUrl + '&' + apiKey,
-    }).done(function (response) {
-        console.log(response);
-    }).fail(function (error) {
-        console.log(error);
-    })
-}
+    const addBackground = () => {
+        const firstSection = $('.firstSection');
+        $.ajax({
+            method: "GET",
+            url: backgroundNasa + '?' + apiKey,
+            dataType: "json",
+        }).done(function (response) {
+            console.log(response);
+            const link = response.hdurl;
+            firstSection.css('backgroundImage', `url(${link})`);
+        });
+        console.log(firstSection);
+        //   firstSection.css('backgroundImage', "url(' + image.hdUrl + ')");
+    }
 
-const addBackground = () => {
-    const firstSection = $('.firstSection');
-    $.ajax({
-        method: "GET",
-        url: backgroundNasa + '?' + apiKey,
-        dataType: "json",
-    }).done(function (response) {
-        console.log(response);
-        const link = response.hdurl;
-        firstSection.css('backgroundImage', `url(${link})`);
-    });
-    console.log(firstSection);
- //   firstSection.css('backgroundImage', "url(' + image.hdUrl + ')");
-}
+    const addGalleryBackgrouds = () => {
+        const secondSection = $('.secondSection');
+        $.ajax({
+            method: "GET",
+            url: nasaUrl + '&' + apiKey,
+            dataType: "json",
+        }).done(function (response) {
+            console.log(response);
+            const link = response.photos[1].img_src;
+            console.log(link);
+            secondsection.css('backgroundImage', `url(${link})`);
+        });
+       // console.log(secondSection);
+        //   firstSection.css('backgroundImage', "url(' + image.hdUrl + ')");
+    }
 
+    loadContent();
+    addBackground();
+    addGalleryBackgrouds();
 
-
-
-
-loadContent();
-addBackground();
+});
