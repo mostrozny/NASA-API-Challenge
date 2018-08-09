a0a39df345f384757a415c0881e9e3da5ace807b
mostrozny <ostrozny.michal@gmail.com>
2018-08-09 16:55:25 +0200

    ajax changes, added photos

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 //api key: uPEwNrk9updnhBTYgoXZiLinsuV9XMoOmyJYSnZi
 
 
-const nasaUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=uPEwNrk9updnhBTYgoXZiLinsuV9XMoOmyJYSnZi'
+const nasaUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000';
+const backgroundNasa = 'https://api.nasa.gov/planetary/apod';
+const apiKey = 'api_key=uPEwNrk9updnhBTYgoXZiLinsuV9XMoOmyJYSnZi';
+let image = '';
 
-function loadContent() {
+const loadContent = () => {
     $.ajax({
-        url: nasaUrl
+        url: nasaUrl + '&' + apiKey,
     }).done(function (response) {
         console.log(response);
     }).fail(function (error) {
@@ -13,4 +16,24 @@ function loadContent() {
     })
 }
 
+const addBackground = () => {
+    const firstSection = $('.firstSection');
+    $.ajax({
+        method: "GET",
+        url: backgroundNasa + '?' + apiKey,
+        dataType: "json",
+    }).done(function (response) {
+        console.log(response);
+        const link = response.hdurl;
+        firstSection.css('backgroundImage', `url(${link})`);
+    });
+    console.log(firstSection);
+ //   firstSection.css('backgroundImage', "url(' + image.hdUrl + ')");
+}
+
+
+
+
+
 loadContent();
+addBackground();
